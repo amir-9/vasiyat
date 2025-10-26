70ab941c66d8daa3f3cee1df8b441be457909853
amir-9 <aghaamirjan13@gmail.com>
2025-10-26 15:09:53 +0330

    updated test landing

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -2,5 +2,9 @@ import { useTranslations } from "next-intl";
 
 export default function HomePage() {
   const t = useTranslations("landing");
-  return <h1>{t("heroTitle")}</h1>;
+  return (
+    <div className="container pt-10">
+      <h1>{t("heroTitle")}</h1>
+    </div>
+  );
 }
