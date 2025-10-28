9ad9af94baa5fa008b9ce4de85abfad1fdbb7c9b
amir-9 <aghaamirjan13@gmail.com>
2025-10-28 19:01:17 +0330

    .

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,10 +1,11 @@
 import Landing from "@/components/home/Landing";
 import HowItWorks from "@/components/home/HowItWorks";
-import WhyWasiyatnameh from "@/components/home/WhyWasiyatnameh";
+import WhyYadegar from "@/components/home/WhyYadegar";
 import Security from "@/components/home/Security";
 import Testimonials from "@/components/home/Testimonials";
 // import Features from "@/components/home/Features";
 import EmotionalSection from "@/components/home/EmotionalSection";
+
 // import FinalCTA from "@/components/home/FinalCTA";
 
 export default function HomePage() {
@@ -12,18 +13,12 @@ export default function HomePage() {
     <div className="w-full overflow-x-hidden">
       <Landing />
       <HowItWorks />
-
-      <WhyWasiyatnameh />
-
+      <WhyYadegar />
       <Security />
-
       <Testimonials />
-
       {/* ✨ ویژگی‌ها */}
       {/* <Features /> */}
-
       <EmotionalSection />
-
       {/* 💌 دعوت به اقدام پایانی */}
       {/* <FinalCTA /> */}
     </div>
